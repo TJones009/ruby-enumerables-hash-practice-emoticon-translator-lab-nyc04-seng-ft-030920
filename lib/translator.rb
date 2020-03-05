bf6ac08a2476179f82140ec800009e9e21135d6a
Trevor Jones <tjones009@yahoo.com>
2020-03-05 16:45:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(file_path, emoticon)
   emoticon_hash = load_library(file_path)
   result = emoticon_hash["get_emoticon"][emoticon]
     if result == nil
-     result = "Sorry, that emoticon was not found :'("
+     result = "Sorry, that emoticon was not found"
     end
   result
 end
@@ -29,7 +29,7 @@ def get_english_meaning(file_path, emoticon)
   emoticon_hash = load_library(file_path)
   result = emoticon_hash["get_meaning"][emoticon]
     if result == nil
-      result = "Sorry, that emoticon was not found :'("
+      result = "Sorry, that emoticon was not found"
     end
     result
 end
